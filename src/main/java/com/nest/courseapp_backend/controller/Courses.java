6c93f5955f89cb8d1f5338e2a120209a656328b6
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-06 11:10:31 +0530

    initial

---
+++ src/main/java/com/nest/courseapp_backend/controller/Courses.java
@@ -0,0 +1,10 @@
+package com.nest.courseapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Courses {
+
+}
